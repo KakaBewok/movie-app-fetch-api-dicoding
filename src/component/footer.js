c3070d912a0eb7310ddd06bfae0b005acdfd0a6d
KkBwk <rizalnov667@gmail.com>
2022-10-01 00:00:36 +0700

    membuat 1 custom element

--- src/component/footer.js
+++ src/component/footer.js
@@ -4,10 +4,22 @@ class FooterElement extends HTMLElement {
   }
 
   render() {
-    this.innerHTML = `<div">
-                            <p class="footer">&copy; Noprizal - 2022</p>
-                        </div>`;
+    this.innerHTML = `  <style>
+                          #footer-element {
+                            display: flex;
+                            justify-content: center;
+                            padding: 4px;
+                          }
+                          #footer-element p a{
+                            text-decoration: none;
+                            color: white;
+                          }
+                        </style>
+                        <div id="footer-element">
+                            <p id="footer"><a href="https://github.com/KakaBewok" target="_blank">&copy; Noprizal - 2022</a></p>
+                        </div>
+                      `;
   }
 }
 
-customElements.define("Footer-Element", FooterElement);
+customElements.define("footer-element", FooterElement);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import main from "./script/main.js";
+import "regenerator-runtime";
 import "./style.css";
+import "./component/footer";
 
 main();
